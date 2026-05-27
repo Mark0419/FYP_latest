fb8c118403c749b05a50e8a36ecc4fb3966081d9
Mark <wnming0419@gmail.com>
2026-05-27 15:27:36 +0800

    change pure_xip tb

--- source/1_legacy_spi/verdiLog/verdi.cmd
+++ source/1_legacy_spi/verdiLog/verdi.cmd
@@ -2,99 +2,84 @@ verdiSetActWin -dock widgetDock_<Decl._Tree>
 wvCreateWindow
 wvSetPosition -win $_nWave2 {("G1" 0)}
 wvOpenFile -win $_nWave2 \
-           {/home/user18/Projects/Flash_controller/source/5_legacy_backup/tb_soc_top.fsdb}
+           {/home/user18/Projects/Flash_controller/FYP_latest/source/1_legacy_spi/tb_soc_top.fsdb}
 verdiSetActWin -win $_nWave2
-verdiWindowResize -win $_Verdi_1 "90" "114" "1757" "700"
+verdiWindowResize -win $_Verdi_1 "90" "77" "1757" "700"
 verdiSetActWin -dock widgetDock_MTB_SOURCE_TAB_1
 verdiSetActWin -win $_nWave2
 wvRestoreSignal -win $_nWave2 \
-           "/home/user18/Projects/Flash_controller/source/5_legacy_backup/signal.rc" \
+           "/home/user18/Projects/Flash_controller/FYP_latest/source/1_legacy_spi/signal.rc" \
            -overWriteAutoAlias on -appendSignals on
 verdiDockWidgetMaximize -dock windowDock_nWave_2
+verdiShowWindow -win $_Verdi_1 -switchFS
+wvSetCursor -win $_nWave2 1765250.736196 -snap {("G5" 3)}
+wvZoom -win $_nWave2 289969.141104 2670768.404908
+wvSetCursor -win $_nWave2 969154.207159 -snap {("G4" 2)}
+wvScrollDown -win $_nWave2 0
+verdiShowWindow -win $_Verdi_1 -switchFS
+verdiWindowResize -win $_Verdi_1 "90" "77" "1757" "700"
+wvOpenFile -win $_nWave2 \
+           {/home/user18/Projects/Flash_controller/FYP_latest/source/2_pure_xip/soc_full.fsdb}
+wvZoomAll -win $_nWave2
 wvGetSignalOpen -win $_nWave2
-wvGetSignalSetScope -win $_nWave2 "/tb_soc_top/u_spi"
-wvGetSignalSetScope -win $_nWave2 "/tb_soc_top/u_spi/u_spi_fsm"
-wvSetPosition -win $_nWave2 {("G4" 7)}
-wvSetPosition -win $_nWave2 {("G4" 7)}
-wvAddSignal -win $_nWave2 -clear
-wvAddSignal -win $_nWave2 -group {"G1" \
-{/tb_soc_top/HCLK} \
-{/tb_soc_top/SYSRESETn} \
-}
-wvAddSignal -win $_nWave2 -group {"G2" \
-{/tb_soc_top/HADDRD\[31:0\]} \
-{/tb_soc_top/HADDRI\[31:0\]} \
-{/tb_soc_top/HRDATAD\[31:0\]} \
-{/tb_soc_top/HTRANSI\[1:0\]} \
-}
-wvAddSignal -win $_nWave2 -group {"G3" \
-{/tb_soc_top/HADDRS\[31:0\]} \
-{/tb_soc_top/HREADYS} \
-{/tb_soc_top/HWDATAS\[31:0\]} \
-{/tb_soc_top/HWRITES} \
-{/tb_soc_top/HRDATAS\[31:0\]} \
-}
-wvAddSignal -win $_nWave2 -group {"G4" \
-{/tb_soc_top/spi_clk} \
-{/tb_soc_top/spi_cs_n} \
-{/tb_soc_top/spi_miso} \
-{/tb_soc_top/spi_mosi} \
-{/tb_soc_top/u_spi/u_spi_fsm/cmd\[7:0\]} \
-{/tb_soc_top/u_spi/u_spi_fsm/flash_addr\[31:0\]} \
-{/tb_soc_top/u_spi/u_spi_fsm/state\[2:0\]} \
-}
-wvAddSignal -win $_nWave2 -group {"G5" \
-}
-wvSelectSignal -win $_nWave2 {( "G4" 5 6 7 )} 
-wvSetPosition -win $_nWave2 {("G4" 7)}
-wvSetPosition -win $_nWave2 {("G4" 5)}
-wvSetPosition -win $_nWave2 {("G4" 6)}
-wvSetPosition -win $_nWave2 {("G4" 7)}
-wvSetPosition -win $_nWave2 {("G5" 0)}
-wvMoveSelected -win $_nWave2
-wvSetPosition -win $_nWave2 {("G5" 3)}
-wvSetPosition -win $_nWave2 {("G5" 3)}
-wvSetPosition -win $_nWave2 {("G5" 3)}
-wvSetPosition -win $_nWave2 {("G5" 3)}
-wvAddSignal -win $_nWave2 -clear
-wvAddSignal -win $_nWave2 -group {"G1" \
-{/tb_soc_top/HCLK} \
-{/tb_soc_top/SYSRESETn} \
-}
-wvAddSignal -win $_nWave2 -group {"G2" \
-{/tb_soc_top/HADDRD\[31:0\]} \
-{/tb_soc_top/HADDRI\[31:0\]} \
-{/tb_soc_top/HRDATAD\[31:0\]} \
-{/tb_soc_top/HTRANSI\[1:0\]} \
-}
-wvAddSignal -win $_nWave2 -group {"G3" \
-{/tb_soc_top/HADDRS\[31:0\]} \
-{/tb_soc_top/HREADYS} \
-{/tb_soc_top/HWDATAS\[31:0\]} \
-{/tb_soc_top/HWRITES} \
-{/tb_soc_top/HRDATAS\[31:0\]} \
-}
-wvAddSignal -win $_nWave2 -group {"G4" \
-{/tb_soc_top/spi_clk} \
-{/tb_soc_top/spi_cs_n} \
-{/tb_soc_top/spi_miso} \
-{/tb_soc_top/spi_mosi} \
-}
-wvAddSignal -win $_nWave2 -group {"G5" \
-{/tb_soc_top/u_spi/u_spi_fsm/cmd\[7:0\]} \
-{/tb_soc_top/u_spi/u_spi_fsm/flash_addr\[31:0\]} \
-{/tb_soc_top/u_spi/u_spi_fsm/state\[2:0\]} \
-}
-wvAddSignal -win $_nWave2 -group {"G6" \
-}
-wvSetPosition -win $_nWave2 {("G5" 3)}
-wvGetSignalClose -win $_nWave2
-wvScrollDown -win $_nWave2 0
-wvScrollDown -win $_nWave2 0
+wvGetSignalSetScope -win $_nWave2 "/sync_fifo"
+wvRestoreSignal -win $_nWave2 \
+           "/home/user18/Projects/Flash_controller/FYP_latest/source/2_pure_xip/signal.rc" \
+           -overWriteAutoAlias on -appendSignals on
+wvZoomAll -win $_nWave2
+wvSelectGroup -win $_nWave2 {G1} {G2} {G3}
+wvSelectSignal -win $_nWave2 {( "G2" 2 )} 
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G2" 1)}
+wvSelectGroup -win $_nWave2 {G1} {G2}
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G4" 4)}
+wvSetPosition -win $_nWave2 {("G3" 0)}
+wvSelectSignal -win $_nWave2 {( "G3" 1 2 3 4 5 )} {( "G4" 1 2 3 4 )} 
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G3" 0)}
+wvSelectSignal -win $_nWave2 {( "G5" 1 2 3 )} 
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G3" 0)}
+wvSelectGroup -win $_nWave2 {G5}
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G3" 0)}
+wvSelectGroup -win $_nWave2 {G4}
+wvSelectGroup -win $_nWave2 {G6}
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G3" 0)}
+wvSelectGroup -win $_nWave2 {G3}
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G4" 0)}
+wvSelectGroup -win $_nWave2 {G4}
+wvCut -win $_nWave2
+wvSetPosition -win $_nWave2 {("G1" 0)}
+wvRestoreSignal -win $_nWave2 \
+           "/home/user18/Projects/Flash_controller/FYP_latest/source/2_pure_xip/signal.rc" \
+           -overWriteAutoAlias on -appendSignals on
+wvZoomAll -win $_nWave2
+wvSetCursor -win $_nWave2 1321009.569697 -snap {("G1" 3)}
+wvZoom -win $_nWave2 570333.483163 1402163.741214
+wvSetCursor -win $_nWave2 625471.288438 -snap {("G1" 5)}
 wvScrollDown -win $_nWave2 0
 wvZoomAll -win $_nWave2
-wvSetCursor -win $_nWave2 6200704.635310 -snap {("G5" 2)}
-wvSetCursor -win $_nWave2 910039.604635 -snap {("G5" 1)}
-wvSaveSignal -win $_nWave2 \
-           "/home/user18/Projects/Flash_controller/source/5_legacy_backup/signal.rc"
-debExit
+wvSetCursor -win $_nWave2 687556.175354 -snap {("G1" 4)}
+wvSetCursor -win $_nWave2 1332280.982407 -snap {("G1" 4)}
+wvSetCursor -win $_nWave2 2824616.025307 -snap {("G1" 4)}
+wvSetCursor -win $_nWave2 2317402.453324 -snap {("G1" 4)}
+wvSetCursor -win $_nWave2 683047.610270 -snap {("G1" 4)}
+wvSetCursor -win $_nWave2 665013.349933 -snap {("G1" 2)}
+wvZoom -win $_nWave2 633453.394343 2910278.761908
+verdiShowWindow -win $_Verdi_1 -switchFS
+wvSetCursor -win $_nWave2 2708901.397088 -snap {("G3" 0)}
+wvZoom -win $_nWave2 659550.003844 1847713.283523
+wvZoomAll -win $_nWave2
+wvSetCursor -win $_nWave2 1435976.598045 -snap {("G3" 0)}
+wvZoom -win $_nWave2 1806749.442904 2935455.727531
+wvSetCursor -win $_nWave2 1846862.779000 -snap {("G1" 2)}
+wvSetCursor -win $_nWave2 1826806.110952 -snap {("G1" 4)}
+verdiShowWindow -win $_Verdi_1 -switchFS
+verdiWindowResize -win $_Verdi_1 "90" "77" "1757" "700"
+wvOpenFile -win $_nWave2 \
+           {/home/user18/Projects/Flash_controller/FYP_latest/source/3_dual_mode/soc_full.fsdb}
+wvZoomAll -win $_nWave2
--- source/2_pure_xip/ahb_wrapper_pure.sv
+++ source/2_pure_xip/ahb_wrapper_pure.sv
@@ -9,7 +9,7 @@ module ahb_wrapper (
     output logic [31:0] HRDATA,
     output logic        HREADYOUT,
     output logic [1:0]  HRESP,
-    
+  
     // QSPI Interface
     output logic        qspi_cs_n, qspi_clk, qspi_data_oen,
     output logic [3:0]  qspi_data_out,
@@ -23,37 +23,35 @@ module ahb_wrapper (
 
     always_ff @(posedge HCLK or negedge HRESETn) begin
         if (!HRESETn) begin
-            pending <= 0; 
-            a_reg <= 0;  
-            fsm_done_q <= 0; 
-            fsm_start <= 0; 
-            ahb_rdata_reg <= 32'h0; 
+            pending       <= 0;
+            a_reg         <= 0;  
+            fsm_done_q    <= 0; 
+            fsm_start     <= 0; 
+            ahb_rdata_reg <= 32'h0;
         end else begin
             fsm_done_q <= fsm_done;
             fsm_start  <= 0;
             
-            // 1. Latch Address Phase (Cycle 1)
-            // Notice: We don't check HWRITE. Whether it's a Read or a Write attempt, 
-            // we latch the address and start the Pure XIP read process (Negative Testing!)
-            if (HSEL && HREADY && HTRANS[1] && !pending) begin
-                pending   <= 1;
-                a_reg     <= HADDR;
-                fsm_start <= 1; 
-            end else if (fsm_done_q) begin
-                pending   <= 0;
-            end
-
-            // 2. Latch Write Data Phase (DELETED)
-            // We no longer need w_data_latch or ahb_wdata_reg because we never write!
-
-            // 3. Latch Read Data (End of transaction)
+            // 1. Latch Read Data (End of FSM transaction)
             if (fsm_done) begin
                 ahb_rdata_reg <= fsm_rx;
+            end
+
+            // 2. Latch Address Phase (Handles Cycle 1 AND Pipelined back-to-back)
+            // By removing !pending, we guarantee we latch the next address the exact 
+            // cycle HREADY goes high, achieving perfect zero-wait-state pipelining.
+            if (HSEL && HREADY && HTRANS[1]) begin
+                pending   <= 1;
+                a_reg     <= HADDR;
+                fsm_start <= 1;
+            end else if (fsm_done_q) begin
+                // If there is no back-to-back sequential transfer, drop pending
+                pending   <= 0;
             end
         end
     end
 
-    // HREADYOUT safely holds the bus using your original pending logic
+    // HREADYOUT safely holds the bus using the pending logic
     assign HREADYOUT = (pending) ? fsm_done_q : 1'b1;
     
     // Always drive the read data back
@@ -61,20 +59,21 @@ module ahb_wrapper (
 
     // Instantiate the Pure XIP FSM instead of the dual-mode one
     qspi_pure_xip u_core (
-        .clk(HCLK), 
-        .rst_n(HRESETn), 
-        .start(fsm_start), 
-        .addr_in(a_reg), 
+        .clk         (HCLK), 
+        .rst_n       (HRESETn), 
+        .start       (fsm_start), 
+        .addr_in     (a_reg), 
         
         // NO write_op, NO tx_data, NO tx_pop
         
-        .done(fsm_done), 
-        .rx_data(fsm_rx),
-        .qspi_cs_n(qspi_cs_n), 
-        .qspi_clk(qspi_clk),
-        .qspi_io_out(qspi_data_out), 
-        .qspi_io_oe(qspi_data_oen), 
-        .qspi_io_in(qspi_data_in)
+        .done        (fsm_done), 
+        .rx_data     (fsm_rx),
+ 
+        .qspi_cs_n   (qspi_cs_n), 
+        .qspi_clk    (qspi_clk),
+        .qspi_io_out (qspi_data_out), 
+        .qspi_io_oe  (qspi_data_oen), 
+        .qspi_io_in  (qspi_data_in)
     );
 
 endmodule
